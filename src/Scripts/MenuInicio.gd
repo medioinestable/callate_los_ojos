57541cf7be6e2a66372e0fda138de9444e026d8a
centroCulturalKappa <anarkxpop@gmail.com>
2021-01-29 04:17:59 -0600

    Agrego menu principal

---
+++ src/Scripts/MenuInicio.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+var state_menu = -1
+onready var menu_ui = $menu_ui
+onready var boton_iniciar = $menu_ui/boton_iniciar_ui
+onready var boton_salir = $menu_ui/boton_salir_ui
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():	
+	boton_iniciar.grab_focus()
+
+
+func _on_boton_iniciar_ui_pressed():
+	print("inicia")
+
+
+func _on_boton_salir_ui_pressed():
+	print("sale")
