361544846226e532b943cc27fc644144ed422701
centroCulturalKappa <anarkxpop@gmail.com>
2021-01-30 22:55:49 -0600

    ya hay nivel 1 despues del tuto

--- src/Scripts/Personaje_1_node.gd
+++ src/Scripts/Personaje_1_node.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 var tiene_objeto = false
-#export(Texture) var textura_objeto
+export(NodePath) var nivel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,3 +22,6 @@ func agarra_objeto(textura_objeto):
 func _on_collision_juntar_area_entered(area):
 	if $'../Personaje_2'.tiene_objeto_2 && tiene_objeto:	
 		print('ganaste!')
+		$'../Personaje_2'.tiene_objeto_2 = false
+		tiene_objeto = false
+		get_node(nivel)._siguiente_nivel()
